8756b94e958ac3c2def40b1f91acbc63f332678a
Thiago Malaquias <tammalaquias@gmail.com>
2022-02-26 18:48:59 -0300

    removendo gem rack-mini-profiler

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   resources :vehicles
   resources :brands
 
-  root to: "vehicles#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "home#index"
 end
