42157a6a15546099bd16dc8112cf2a81d79353ce
Thiago Malaquias <tammalaquias@gmail.com>
2022-02-26 21:19:11 -0300

    finalizando layout de veiculos

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -2,9 +2,11 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*',
+    "./app/**/*.html.erb",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/javascript/**/*.vue",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {
@@ -17,5 +19,6 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
+    require('flowbite/plugin')
   ]
 }
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+4.times do
+  brand = Brand.create(name: Faker::Vehicle.manufacture)
+
+  5.times do
+    Vehicle.create(
+      brand: brand,
+      name: Faker::Vehicle.model,
+      color: Faker::Color.hex_color,
+      qty_passengers: Faker::Number.number(digits: 1),
+      plate: Faker::Vehicle.license_plate,
+      value: Faker::Number.decimal(l_digits: 3, r_digits: 2)
+    )
+  end
+end
